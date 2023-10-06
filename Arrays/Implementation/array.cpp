e230263cbba60b5b09e69d0c8149315dd072f7b4
umamaheshdev01 <139320517+umamaheshdev01@users.noreply.github.com>
2023-10-07 00:31:44 +0530

    Hi

--- Arrays/Implementation/array.cpp
+++ Arrays/Implementation/array.cpp
@@ -3,9 +3,55 @@ using namespace std;
 
 class Array{
 
+    //values
+    private:
+        int size;
+        int * A;
+        int index=0;
+    
+    public:
+    //constructor
+    Array(int x)
+    {
+        size=x;
+        A = new int[x];
+    }
+
+    //Insertion
+    void insert(int x)
+    {
+        if(index>=size)
+        {
+            cout<<"Overflow"<<endl;
+        }
+        else
+        {
+            A[index++] = x;
+        }
+    }
+
+    //Display
+    void display()
+    {
+        if(index==0)
+        {
+            cout<<"Array is empty";
+            return;
+        }
+
+        for(int i=0;i<index;i++)
+        {
+            cout<<A[i]<<" ";
+        }
+
+        cout<<endl;
+    }
+
 };
 
 int main()
 {
-    cout<<"Hi";
+    Array arr(10);
+    arr.display();
+
 }
